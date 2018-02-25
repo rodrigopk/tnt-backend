d4a52eecc5ee490aa4befe630c9e876a8469d61b
rodrigopk <rodrigopk@gmail.com>
2018-02-25 23:38:14 +0100

    Add hello action

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,4 +1,4 @@
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
 #
-get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+get '/hello', to: 'hello#index'
---
+++ apps/api/controllers/hello/index.rb
@@ -0,0 +1,13 @@
+module Api
+  module Controllers
+    module Hello
+      class Index
+        include Api::Action
+
+        def call(_params)
+          status 200, { message: 'Hello from Hanami!' }.to_json
+        end
+      end
+    end
+  end
+end
