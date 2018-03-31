0570811f4d13e1956c37ea273bbb5c778de79f22
rodrigopk <rodrigopk@gmail.com>
2018-03-31 10:33:01 +0200

    Fix minimal test for hello/index

--- spec/api/controllers/hello/index_spec.rb
+++ spec/api/controllers/hello/index_spec.rb
@@ -8,6 +8,6 @@ describe Api::Controllers::Hello::Index do
 
   it 'is successful' do
     response = action.call(params)
-    response[0].must_equal 200
+    expect(response[0]).to be 200
   end
 end
