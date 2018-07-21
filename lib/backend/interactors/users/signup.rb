a4f3bfca746052a9ab311ace9a9b62b00cba62e7
rodrigopk <rodrigopk@gmail.com>
2018-07-21 09:20:43 +0200

    Use encrypted password with user

--- lib/backend/interactors/users/signup.rb
+++ lib/backend/interactors/users/signup.rb
@@ -11,10 +11,30 @@ module Interactors
         @user_repository = dependencies.fetch(:repository) do
           UserRepository.new
         end
+
+        @password_service = dependencies.fetch(:password_service) do
+          BCrypt::Password
+        end
       end
 
       def call(params)
-        @user = @user_repository.create(params)
+        @user = @user_repository.create(
+          user_params_with_password_digest(params),
+        )
+      end
+
+      private
+
+      def user_params_with_password_digest(user_params)
+        {
+          name: user_params[:name],
+          email: user_params[:email],
+          password_digest: encrypted_password(user_params[:password]),
+        }
+      end
+
+      def encrypted_password(password)
+        @password_service.create(password)
       end
     end
   end
--- spec/backend/interactors/users/signup_spec.rb
+++ spec/backend/interactors/users/signup_spec.rb
@@ -4,20 +4,36 @@ require_relative '../../../spec_helper'
 
 describe Interactors::Users::Signup do
   let(:repository) { instance_double(UserRepository) }
-  let(:interactor) { described_class.new(repository: repository) }
+  let(:password_service) { class_double(BCrypt::Password) }
+  let(:interactor) do
+    described_class.new(
+      repository: repository,
+      password_service: password_service,
+    )
+  end
 
   let(:user_params) { build_user_params }
+  let(:encrypted_password) { 'super-secure-encrypted-password-hash' }
   let(:user) { instance_double(User) }
 
   describe '#call' do
     it 'creates an user' do
-      expect(repository).to receive(:create).with(user_params).and_return(user)
+      allow(password_service).to encrypt_user_password
+      expect(repository).to create_new_user
+
+      interactor.call(user_params)
+    end
+
+    it 'encrypts the user password' do
+      expect(password_service).to encrypt_user_password
+      allow(repository).to create_new_user
 
       interactor.call(user_params)
     end
 
     it 'exposes the created user' do
-      allow(repository).to receive(:create).with(user_params).and_return(user)
+      allow(password_service).to encrypt_user_password
+      allow(repository).to create_new_user
 
       result = interactor.call(user_params)
       expect(result.user).to eq(user)
@@ -30,7 +46,25 @@ describe Interactors::Users::Signup do
     {
       name: 'Penelope Cruz',
       email: 'penelope@cruz.com',
-      password: 'p4sswrD',
+      password: 'p4sswrD!',
+    }
+  end
+
+  def create_new_user
+    receive(:create)
+      .with(user_params_with_password_digest)
+      .and_return(user)
+  end
+
+  def encrypt_user_password
+    receive(:create).and_return(encrypted_password)
+  end
+
+  def user_params_with_password_digest
+    {
+      name: user_params[:name],
+      email: user_params[:email],
+      password_digest: encrypted_password,
     }
   end
 end
